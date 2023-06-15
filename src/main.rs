a96dba236ad0e0a5da9b3d322f9d64d5e6aff705
Skyf0l <tom.rorato@epitech.eu>
2023-06-15 19:27:14 +0200

    feat: poc rsa decryption with rug

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,42 @@
-use clap::{command, Parser, Subcommand};
+use clap::{command, Parser};
 use main_error::MainError;
 
-use rsacracker::attack;
+use rug::Integer;
 
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about)]
 struct Args {
     #[clap(flatten)]
     options: Options,
-    /// Subcommands.
-    #[command(subcommand)]
-    subcommand: SubCommand,
 }
 
 /// Options.
 #[derive(Parser, Debug, Clone)]
-struct Options {}
-
-/// Subcommands.
-#[derive(Subcommand, Debug, Clone)]
-enum SubCommand {}
+struct Options {
+    /// Prime number p.
+    #[clap(short)]
+    p: Integer,
+    /// Prime number q.
+    #[clap(short)]
+    q: Integer,
+    /// Public exponent.
+    #[clap(short)]
+    e: Integer,
+    /// Cipher message.
+    #[clap(short)]
+    c: Integer,
+}
 
 #[cfg(not(tarpaulin_include))]
 fn main() -> Result<(), MainError> {
     let _args = Args::parse();
 
-    attack();
+    let n = _args.options.p.clone() * _args.options.q.clone();
+    let e = _args.options.e;
+    let phi = (_args.options.p - 1) * (_args.options.q - 1);
+    let d = e.invert(&phi).unwrap();
+    let m = _args.options.c.pow_mod(&d, &n).unwrap();
+    println!("m = {}", m);
 
     Ok(())
 }

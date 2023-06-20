8de66cc4781cbfd292b9ab3921b6805695a7012b
Skyf0l <tom.rorato@epitech.eu>
2023-06-20 18:37:24 +0200

    feat: accept hex, octal, binary and decimal numbers as CLI input

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,60 @@ use rug::Integer;
 
 use rsacracker::{run_attacks, Parameters};
 
+#[derive(Debug, Clone)]
+struct IntegerArg(Integer);
+
+impl std::str::FromStr for IntegerArg {
+    type Err = String;
+
+    fn from_str(n: &str) -> Result<Self, Self::Err> {
+        if n.starts_with("0x") {
+            Ok(Self(
+                Integer::from_str_radix(&n[2..], 16).or(Err("Invalid hex number".to_string()))?,
+            ))
+        } else if n.starts_with("0b") {
+            Ok(Self(
+                Integer::from_str_radix(&n[2..], 2).or(Err("Invalid binary number".to_string()))?,
+            ))
+        } else if n.starts_with("0o") {
+            Ok(Self(
+                Integer::from_str_radix(&n[2..], 8).or(Err("Invalid octal number".to_string()))?,
+            ))
+        } else {
+            Ok(Self(
+                Integer::from_str(n).or(Err("Invalid number".to_string()))?,
+            ))
+        }
+    }
+}
+
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about)]
 struct Args {
     /// Modulus.
     #[clap(short)]
-    n: Option<Integer>,
+    n: Option<IntegerArg>,
     /// Prime number p.
     #[clap(short)]
-    p: Option<Integer>,
+    p: Option<IntegerArg>,
     /// Prime number q.
     #[clap(short)]
-    q: Option<Integer>,
+    q: Option<IntegerArg>,
     /// dP or dmp1 CRT exponent. (d mod p-1)
     #[clap(long)]
-    dp: Option<Integer>,
+    dp: Option<IntegerArg>,
     /// dQ or dmq1 CRT exponent. (d mod q-1)
     #[clap(long)]
-    dq: Option<Integer>,
+    dq: Option<IntegerArg>,
     /// The sum of the two primes p and q.
     #[clap(long)]
-    sum_pq: Option<Integer>,
+    sum_pq: Option<IntegerArg>,
     /// Public exponent. Default: 65537
     #[clap(short, default_value = "65537")]
-    e: Integer,
+    e: IntegerArg,
     /// Cipher message.
     #[clap(short)]
-    c: Option<Integer>,
+    c: Option<IntegerArg>,
     /// Public key PEM file.
     #[clap(long)]
     publickey: Option<String>,
@@ -55,14 +82,14 @@ fn main() -> Result<(), MainError> {
         Parameters::from_publickey(&bytes).ok_or("Invalid public key")?
     } else {
         Parameters {
-            n: args.n,
-            p: args.p,
-            q: args.q,
-            dp: args.dp,
-            dq: args.dq,
-            sum_pq: args.sum_pq,
-            e: args.e,
-            c: args.c.clone(),
+            n: args.n.map(|n| n.0),
+            p: args.p.map(|n| n.0),
+            q: args.q.map(|n| n.0),
+            dp: args.dp.map(|n| n.0),
+            dq: args.dq.map(|n| n.0),
+            sum_pq: args.sum_pq.map(|n| n.0),
+            e: args.e.0,
+            c: args.c.map(|n| n.0),
         }
     };
     let (_private_key, uncipher) = run_attacks(&params).ok_or("No attack succeeded")?;

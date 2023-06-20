35310b1d711002acde827cdc18387a4f8684d81d
Skyf0l <tom.rorato@epitech.eu>
2023-06-21 00:07:52 +0200

    fix: panic on ecm + bump packages

--- src/attack/ecm.rs
+++ src/attack/ecm.rs
@@ -13,7 +13,11 @@ impl Attack for EcmAttack {
         let e = &params.e;
         let n = params.n.as_ref().ok_or(Error::MissingParameters)?;
 
-        let factors = ecm::ecm(n).iter().cloned().collect::<Vec<_>>();
+        let factors = ecm::ecm(n)
+            .or(Err(Error::NotFound))?
+            .iter()
+            .cloned()
+            .collect::<Vec<_>>();
         if factors.len() < 2 {
             return Err(Error::NotFound);
         }

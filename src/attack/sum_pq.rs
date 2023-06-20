2666ccdea33fb073670542f5ab8a6b03af4e4ec2
Skyf0l <tom.rorato@epitech.eu>
2023-06-20 13:46:13 +0200

    fix: z3 equations

--- src/attack/sum_pq.rs
+++ src/attack/sum_pq.rs
@@ -23,20 +23,20 @@ impl Attack for SumPQAttack {
         let ctx = z3::Context::new(&cfg);
 
         // Z3 variables
-        let product: z3::ast::Int<'_> = z3::ast::Int::from_str(&ctx, &n.to_string()).unwrap();
-        let sum = z3::ast::Int::from_str(&ctx, &sum_pq.to_string()).unwrap();
-        let p = z3::ast::Int::new_const(&ctx, "p");
-        let q = z3::ast::Int::new_const(&ctx, "q");
+        let n = &z3::ast::Int::from_str(&ctx, &n.to_string()).unwrap();
+        let sum = &z3::ast::Int::from_str(&ctx, &sum_pq.to_string()).unwrap();
+        let p = &z3::ast::Int::new_const(&ctx, "p");
+        let q = &z3::ast::Int::new_const(&ctx, "q");
         let one = &z3::ast::Int::from_i64(&ctx, 1);
 
         // Z3 constraints
         let solver = z3::Solver::new(&ctx);
-        solver.assert(&(&p + &q)._eq(&sum));
-        solver.assert(&(&p * &q)._eq(&product));
-        solver.assert(&product.gt(&p));
-        solver.assert(&product.gt(&q));
-        solver.assert(&p.gt(&one));
-        solver.assert(&q.gt(&one));
+        solver.assert(&(p + q)._eq(sum));
+        solver.assert(&(p * q)._eq(n));
+        solver.assert(&n.gt(p));
+        solver.assert(&n.gt(q));
+        solver.assert(&p.gt(one));
+        solver.assert(&q.gt(one));
 
         if solver.check() != z3::SatResult::Sat {
             return Err(Error::NotFound);
@@ -44,8 +44,8 @@ impl Attack for SumPQAttack {
 
         // Z3 solution
         let model = solver.get_model().unwrap();
-        let p = model.eval(&p, true).ok_or(Error::NotFound)?;
-        let q = model.eval(&q, true).ok_or(Error::NotFound)?;
+        let p = model.eval(p, true).ok_or(Error::NotFound)?;
+        let q = model.eval(q, true).ok_or(Error::NotFound)?;
 
         let p = Integer::from_str(&p.to_string()).unwrap();
         let q = Integer::from_str(&q.to_string()).unwrap();
--- src/attack/z3.rs
+++ src/attack/z3.rs
@@ -23,24 +23,24 @@ impl Attack for Z3Attack {
         let ctx = z3::Context::new(&cfg);
 
         // Z3 variables
-        let n = z3::ast::Int::from_str(&ctx, &n.to_string()).unwrap();
-        let i = z3::ast::Int::from_str(&ctx, &i.to_string()).unwrap();
-        let p = z3::ast::Int::new_const(&ctx, "p");
-        let q = z3::ast::Int::new_const(&ctx, "q");
+        let n = &z3::ast::Int::from_str(&ctx, &n.to_string()).unwrap();
+        let i = &z3::ast::Int::from_str(&ctx, &i.to_string()).unwrap();
+        let p = &z3::ast::Int::new_const(&ctx, "p");
+        let q = &z3::ast::Int::new_const(&ctx, "q");
         let one = &z3::ast::Int::from_i64(&ctx, 1);
         let two = &z3::ast::Int::from_i64(&ctx, 2);
 
         // Z3 constraints
         let solver = z3::Solver::new(&ctx);
-        solver.assert(&(&p * &q)._eq(&n));
-        solver.assert(&n.gt(&p));
-        solver.assert(&n.gt(&q));
-        solver.assert(&p.ge(&i));
-        solver.assert(&q.lt(&i));
-        solver.assert(&p.gt(&one));
-        solver.assert(&q.gt(&one));
-        solver.assert(&q.modulo(&two)._eq(&one));
-        solver.assert(&p.modulo(&two)._eq(&one));
+        solver.assert(&(p * q)._eq(n));
+        solver.assert(&n.gt(p));
+        solver.assert(&n.gt(q));
+        solver.assert(&p.ge(i));
+        solver.assert(&q.lt(i));
+        solver.assert(&p.gt(one));
+        solver.assert(&q.gt(one));
+        solver.assert(&q.modulo(two)._eq(one));
+        solver.assert(&p.modulo(two)._eq(one));
 
         if solver.check() != z3::SatResult::Sat {
             return Err(Error::NotFound);
@@ -48,8 +48,8 @@ impl Attack for Z3Attack {
 
         // Z3 solution
         let model = solver.get_model().unwrap();
-        let p = model.eval(&p, true).ok_or(Error::NotFound)?;
-        let q = model.eval(&q, true).ok_or(Error::NotFound)?;
+        let p = model.eval(p, true).ok_or(Error::NotFound)?;
+        let q = model.eval(q, true).ok_or(Error::NotFound)?;
 
         let p = Integer::from_str(&p.to_string()).unwrap();
         let q = Integer::from_str(&q.to_string()).unwrap();

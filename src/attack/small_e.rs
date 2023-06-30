e3967d907c3b11c6052f0fd81003f6a0d3325e7b
Skyf0l <tom.rorato@epitech.eu>
2023-06-30 17:56:09 +0200

    chore: run small_e attack for all e

--- src/attack/small_e.rs
+++ src/attack/small_e.rs
@@ -1,6 +1,4 @@
-use std::ops::Mul;
-
-use rug::{ops::Pow, Integer};
+use rug::Integer;
 
 use crate::{Attack, Error, Parameters, SolvedRsa};
 
@@ -14,19 +12,16 @@ impl Attack for SmallEAttack {
     }
 
     fn run(&self, params: &Parameters) -> Result<SolvedRsa, Error> {
-        if params.e != 3 && params.e != 5 {
-            return Err(Error::NotFound);
-        }
         let e = params.e.to_u32().unwrap();
         let n = params.n.as_ref().ok_or(Error::MissingParameters)?;
         let c = params.c.as_ref().ok_or(Error::MissingParameters)?;
 
         for i in 1..10000 {
-            let enc = Mul::<Integer>::mul(n, i.into()) + c.clone();
-            let root = enc.clone().root(e);
+            let enc = Integer::from(n) * Integer::from(i) + c.clone();
+            let (root, rem) = enc.root_rem_ref(e).into();
 
             // If the root is perfect, we found the plaintext
-            if root.clone().pow(e) == enc {
+            if rem == Integer::ZERO {
                 return Ok((None, Some(root)));
             }
         }
